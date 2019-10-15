6f12fbffd70375638aec5a32c03f046b1753c7e6
piratelicorne <thecodeloops@gmail.com>
2019-10-14 17:18:36 -0700

    routes linked to every page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+
+import * as ROUTES from '../../constants/routes';
 
 const App = () => (
-    <div>
-        <h1>App</h1>
-    </div>
+    <Router>
+        <Navigation/>
+        <hr />
+
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+    </Router>
 );
 
 export default App;
