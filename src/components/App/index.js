21b96f6c6bf1ac6bcd031fc61658333fc05002e4
piratelicorne <thecodeloops@gmail.com>
2019-11-13 07:08:09 -0800

    NOW YOU CAN SIGN OUT, navigation changes according to log status

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,5 @@
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-} from 'react-router-dom';
-
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation';
 import AboutUsPage from '../AboutUs';
@@ -15,28 +11,49 @@ import AccountPage from '../Account';
 import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
-import { FirebaseContext } from '../Firebase';
+import { withFirebase } from '../Firebase';
 
-const App = () => (
-    <FirebaseContext>
-        {firebase => {
-            return  <Router>
-                        <Navigation/>
-                        <hr />
-                        <div style={{ padding: "60px 60px", textAlign: "center", color: "grey"}}> 
-                            <header>EDHEC FAIRTRADE PROJECT</header>
-                            <Route exact path={ROUTES.ABOUT_US} component={AboutUsPage} />
-                            <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-                            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-                            <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-                            <Route path={ROUTES.HOME} component={HomePage} />
-                            <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-                            <Route path={ROUTES.ADMIN} component={AdminPage} />
-                        </div>
-                   
-                    </Router>
-        }}
-    </FirebaseContext>
-);
+//change const App to class App
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-export default App;
+        this.state = {
+            authUser: null,
+        };
+    }
+
+    componentDidMount() {
+        this.listener = this.props.firebase.auth.onAuthStateChanged(
+            authUser => {
+                authUser
+                    ? this.setState({ authUser })
+                    : this.setState({ authUser: null });
+        });
+    }
+
+    componentWillUnmount() {
+        this.listener();
+    }
+
+    render() {
+        return (
+            <Router>
+                <Navigation authUser={this.state.authUser}/>
+                <hr />
+                <div style={{ padding: "60px 60px", textAlign: "center", color: "grey"}}> 
+                    <header>EDHEC FAIRTRADE PROJECT</header>
+                    <Route exact path={ROUTES.ABOUT_US} component={AboutUsPage} />
+                    <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+                    <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+                    <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+                    <Route path={ROUTES.HOME} component={HomePage} />
+                    <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+                    <Route path={ROUTES.ADMIN} component={AdminPage} />
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default withFirebase(App);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 
-//import { FirebaseContext } from '../Firebase';
+//navigation costumize according to log status
+const Navigation = ({ authUser }) => (
+    <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+);
 
-const Navigation = () => (
+const NavigationAuth = () => (
     <Navbar bg="dark" variant="dark">
         <div>
             <Nav className="nav-item">
@@ -34,5 +38,24 @@ const Navigation = () => (
         </div>
     </Navbar>
 );
+
+const NavigationNonAuth = () => (
+    <Navbar bg="dark" variant="dark">
+        <div>
+            <Nav className="nav-item">
+                <li>
+                    <Link class = "nav-link" to ={ROUTES.HOME}>Home</Link>
+                </li>
+                <li>
+                    <Link class = "nav-link" to={ROUTES.ABOUT_US}>About Us</Link>
+                </li>
+                <li>
+                    <Link class = "nav-link" to={ROUTES.SIGN_IN}>Sign In</Link>
+                </li>
+            </Nav>  
+        </div>
+    </Navbar>
+);
+
 
 export default Navigation;
