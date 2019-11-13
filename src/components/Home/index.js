5511a3ed1cbb2339487adc9d946ad012edabc543
vehernan <hveneranda@yahoo.com>
2019-11-13 07:28:33 -0800

    added image and volunteer button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,10 +6,13 @@ const Home = () => (
     <FirebaseContext.Consumer>
         {firebase => {
             return <div>
+                <img src="https://picsum.photos/id/425/959/635"/>
                         <h1>WHY FAIR TRADE?</h1>
                         <p>Fair trade is a global movement made up of a diverse network of producers, companies, shoppers, advocates, and organizations putting people and planet first.
 
 We at Fair Trade USA® believe that everyone wants to do what’s right – for their families, fellow global citizens, and the planet. That’s why we’re committed to making the right choices the most obvious ones. Based on the simple idea that the products we buy and sell are connected to the livelihoods of others, fair trade is a way to make a conscious choice for a better world. A choice for Fair Trade Certified™ goods is a choice to support responsible companies, empower farmers, workers, and fishermen, and protect the environment. In other words, it’s a world-changing way of doing business.</p>
+            <h2> HERE'S HOW YOU CAN HELP!</h2>
+            <button onClick="myfunction()">VOLUNTEER</button>
                     </div>
         }}
     </FirebaseContext.Consumer>
