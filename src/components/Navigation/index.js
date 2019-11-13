68b892752359dd3e4f9ced7d083ceef553a9a7d5
csalceda88@gmail.com <csalceda88@gmail.com>
2019-11-13 04:03:44 -0800

    modified navigation bar code, organization, and style with bootstrap

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
 
 import * as ROUTES from '../../constants/routes';
 
@@ -8,25 +11,15 @@ import { FirebaseContext } from '../Firebase';
 const Navigation = () => (
     <FirebaseContext.Consumer>
         {firebase => {
-            return  <div>
-                        <ul>
-                            <li>
-                                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-                            </li>
-                            <li>
-                                <Link to={ROUTES.LANDING}>Landing</Link>
-                            </li>
-                            <li>
-                                <Link to ={ROUTES.HOME}>Home</Link>
-                            </li>
-                            <li>
-                                <Link to ={ROUTES.ACCOUNT}>Account</Link>
-                            </li>
-                            <li>
-                                <Link to ={ROUTES.ADMIN}>Admin</Link>
-                            </li>
-                        </ul>
-                    </div>
+            return  <Navbar bg="dark" variant="dark">
+                        <Nav className="nav-item">
+                            <Link class = "nav-link" to ={ROUTES.HOME}>Home</Link>
+                            <Link class = "nav-link" to={ROUTES.ABOUT_US}>About Us</Link>
+                            <Link class = "nav-link" to ={ROUTES.ACCOUNT}>Account</Link>
+                            <Link class = "nav-link" to ={ROUTES.ADMIN}>Admin</Link>
+                            <Link class = "nav-link" to={ROUTES.SIGN_IN}>Sign In</Link>
+                        </Nav>  
+                    </Navbar>
         }}
     </FirebaseContext.Consumer>
    
