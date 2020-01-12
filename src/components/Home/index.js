05d70720b9908ef1ca4bcb22e4122f7b45ea6c2e
vehernan <hveneranda@yahoo.com>
2020-01-11 17:09:03 -0800

    fixed the country search bar now it shows

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 
 import { FirebaseContext } from '../Firebase';
 
-function handleClick() {
-    var b = document.getElementById("button1");
-    b.innerText = "Clicked"
-    console.log("I was clicked")
-}
-
 const Home = () => (
     <FirebaseContext.Consumer>
         {firebase => {
@@ -18,7 +12,6 @@ const Home = () => (
 
 We at Fair Trade USA® believe that everyone wants to do what’s right – for their families, fellow global citizens, and the planet. That’s why we’re committed to making the right choices the most obvious ones. Based on the simple idea that the products we buy and sell are connected to the livelihoods of others, fair trade is a way to make a conscious choice for a better world. A choice for Fair Trade Certified™ goods is a choice to support responsible companies, empower farmers, workers, and fishermen, and protect the environment. In other words, it’s a world-changing way of doing business.</p>
             <h2> HERE'S HOW YOU CAN HELP!</h2>
-            <button id = "button1" onClick={handleClick}>VOLUNTEER</button>
                     </div>
         }}
     </FirebaseContext.Consumer>
