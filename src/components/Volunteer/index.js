b93fb13cd15c9e4c4be42d52fb343bfac3f3fa96
csalceda88@gmail.com <csalceda88@gmail.com>
2020-01-12 01:15:42 -0800

    volunteer changes

--- src/components/Volunteer/index.js
+++ src/components/Volunteer/index.js
@@ -29,7 +29,7 @@ const Volunteer = () => (
                     </div>
                     <div class="field">
                         <div class="ui checkbox">
-                        <input type="checkbox" tabindex="0" class="hidden" />
+                        <input type="checkbox" tabindex="0" />
                         <label>I agree to the Terms and Conditions</label>
                         </div>
                     </div>
@@ -374,26 +374,17 @@ const Volunteer = () => (
 
                     <h4 class="ui dividing header">Billing Information</h4>
                     <div class="field">
-                        <label>Card Type</label>
-                        <div class="ui selection dropdown">
-                        <input type="hidden" name="card[type]"/>
-                        <div class="default text">Type</div>
-                        <i class="dropdown icon"></i>
-                        <div class="menu">
-                            <div class="item" data-value="visa">
-                            <i class="visa icon"></i>
-                            Visa
+                            <div class="field">
+                                <label>Card Type</label>
+                                <input type="hidden" name="card[type]" />
+                                <select class="ui selection dropdown" id="select">
+                            <option>Type</option>
+                            <option>Visa</option>
+                            <option>American Express</option>
+                            <option>Mastercard</option>
+                            <option>Discover</option>
+                            </select>
                             </div>
-                            <div class="item" data-value="amex">
-                            <i class="amex icon"></i>
-                            American Express
-                            </div>
-                            <div class="item" data-value="discover">
-                            <i class="discover icon"></i>
-                            Discover
-                            </div>
-                        </div>
-                        </div>
                     </div>
                     <div class="fields">
                         <div class="seven wide field">
@@ -430,45 +421,11 @@ const Volunteer = () => (
                         </div>
                         </div>
                     </div>
-                    <h4 class="ui dividing header">Receipt</h4>
-                    <div class="field">
-                        <label>Send Receipt To:</label>
-                        <div class="ui fluid multiple search selection dropdown">
-                        <input type="hidden" name="receipt" />
-                        <i class="dropdown icon"></i>
-                        <div class="default text">Saved Contacts</div>
-                        <div class="menu">
-                            <div class="item" data-value="jenny" data-text="Jenny">
-                            <img class="ui mini avatar image" src="/images/avatar/small/jenny.jpg" />
-                            Jenny Hess
-                            </div>
-                            <div class="item" data-value="elliot" data-text="Elliot">
-                            <img class="ui mini avatar image" src="/images/avatar/small/elliot.jpg" />
-                            Elliot Fu
-                            </div>
-                            <div class="item" data-value="stevie" data-text="Stevie">
-                            <img class="ui mini avatar image" src="/images/avatar/small/stevie.jpg" />
-                            Stevie Feliciano
-                            </div>
-                            <div class="item" data-value="christian" data-text="Christian">
-                            <img class="ui mini avatar image" src="/images/avatar/small/christian.jpg" />
-                            Christian
-                            </div>
-                            <div class="item" data-value="matt" data-text="Matt">
-                            <img class="ui mini avatar image" src="/images/avatar/small/matt.jpg" />
-                            Matt
-                            </div>
-                            <div class="item" data-value="justen" data-text="Justen">
-                            <img class="ui mini avatar image" src="/images/avatar/small/justen.jpg" />
-                            Justen Kitsune
-                            </div>
-                        </div>
-                        </div>
-                    </div>
+                    
                     <div class="ui segment">
                         <div class="field">
                         <div class="ui toggle checkbox">
-                            <input type="checkbox" name="gift" tabindex="0" class="hidden" />
+                            <input type="checkbox" name="gift" tabindex="0" />
                             <label>Do not include a receipt in the package</label>
                         </div>
                         </div>
