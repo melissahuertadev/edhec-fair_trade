92e82fca01a9092642ea4c514901e6c5b1e2f55b
csalceda88@gmail.com <csalceda88@gmail.com>
2020-01-13 01:29:18 -0800

    .

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -5,10 +5,72 @@ import { FirebaseContext } from '../Firebase';
 const Admin = () => (
     <FirebaseContext.Consumer>
         {firebase => {
-            return <div>
+            return (
+            <div>
                 <h1>Admin</h1>
+                <div class="ui vertical menu">
+                <div class="item">
+                    <div class="header">Products</div>
+                    <div class="menu">
+                    <a class="item">Product1</a>
+                    <a class="item">Product2</a>
+                    <a class="item">Product3</a>
+                    <a class="item">Product4</a>
+                    <a class="item">Product5</a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="header">Partnerships</div>
+                    <div class="menu">
+                    <a class="item">Company1</a>
+                    <a class="item">Company2</a>
+                    <a class="item">Company3</a>
+                    <a class="item">Company4</a>
+                    <a class="item">Company5</a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="header">Authorized Employees</div>
+                    <div class="menu">
+                    <a class="item">User1</a>
+                    <a class="item">User2</a>
+                    <a class="item">User3</a>
+                    <a class="item">User4</a>
+                    <a class="item">User5</a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="header">Payment Methods</div>
+                    <div class="menu">
+                    <a class="item">Payment1</a>
+                    <a class="item">Payment2</a>
+                    <a class="item">Payment3</a>
+                    <a class="item">Payment4</a>
+                    <a class="item">Payment5</a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="header">Settings</div>
+                    <div class="menu">
+                    <a class="item">Add/Delete Products</a>
+                    <a class="item">Add/Delete Company</a>
+                    <a class="item">Add/Delete Employees</a>
+                    <a class="item">Edit My Payment Methods</a>
+                    <a class="item">Edit My Info</a>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="header">Support</div>
+                    <div class="menu">
+                    <a class="item">E-mail Support</a>
+                    <a class="item">FAQs</a>
+                    </div>
+                </div>
+                </div>
             </div>
-        }}
+        )
+        }
+    }
     </FirebaseContext.Consumer>
 );
 
